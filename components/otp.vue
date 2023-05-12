a4eeab760fd0cdb97e5cdeceb118bc412f73b6c3
satyam soni <ssoni@greenstreet.com>
2023-05-12 16:26:08 +0530

    Added Backdspace for OTP component

--- components/otp.vue
+++ components/otp.vue
@@ -3,7 +3,8 @@
         <input type="text"
             class="border-1 border-solid border-gray-6 border-rounded-2 w-8 h-9 mx-2 px-auto text-center shadow-md"
             autocomplete="one-time-code" pattern="\d{1}" maxlength="1" inputmode="numeric" ref="`o${i}"
-            @input="$event => handleChange($event, i)" v-for="i of length">
+            @input="$event => handleChange($event, i)" v-for="i of length"
+            @keydown.delete="$event => handlePrevious($event, i)" :value="inputList[i - 1]" @focus="$event.target.select()">
 
     </div>
 </template>
@@ -17,9 +18,6 @@ export default {
             inputList.push('')
         }
         return { inputList: ref(inputList) }
-    },
-    mounted() {
-        console.log(this.inputList)
     },
     props: {
         length: {
@@ -44,6 +42,19 @@ export default {
                 event.target.blur()
                 // emit this opt when all the input boxes are filled
                 this.$emit('otp', this.inputList.join(''))
+            }
+        },
+        handlePrevious(event, index) {
+            this.inputList[index - 1] = ''
+            event.target.value = ''
+            /**
+             * @type {HTMLInputElement}
+             */
+            const previousElement = event.target.previousElementSibling;
+            if (previousElement) {
+                previousElement.select()
+            } else {
+                event.target.blur()
             }
         }
     }
